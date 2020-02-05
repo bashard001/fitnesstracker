ba44c03cf2e4a863cffc766af476dc53a7e5efbf
bashard001 <bdaseh@gmail.com>
2020-02-04 18:29:53 -0800

    working on setting up the api routes and creating mongoDB connection

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const workout = require("../models/workout");
+
+router.post("/api/workouts", ({ body }, res) => {
+  workout.create(body)
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workout/range", ({ body }, res) => {
+  workout.insertMany(body)
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  workout.find({})
+    .sort({ date: -1 })
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
