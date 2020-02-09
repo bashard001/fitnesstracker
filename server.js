f417ac98328265d49aa4d0b44e423a7dea2f3016
bashard001 <bdaseh@gmail.com>
2020-02-08 16:58:36 -0800

    mlab mongodb setup

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 // mongodb://heroku_vtp7kv56:d6mvgj91mvkcvatgaahqc47ppm@ds061681.mlab.com:61681/heroku_vtp7kv56
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout" || "mongodb://user1:password1@ds061681.mlab.com:61681/heroku_vtp7kv56", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/workout";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
